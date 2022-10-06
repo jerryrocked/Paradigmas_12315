e9bfe58a064fb7e45032bd07f0c9aa1177e7bcbe
dylanMauro <91442811+dylanMauro@users.noreply.github.com>
2022-10-05 21:21:26 -0300

    Update main.cpp

--- TAREA 1 - DylanCornejo_ChristianRojas/main.cpp
+++ TAREA 1 - DylanCornejo_ChristianRojas/main.cpp
@@ -2,11 +2,11 @@
 
 int main(){
 	// Inicializacion de los objetos y objetos de clases derivadas
-	Rectangulo r(400, 300, 5, "Rectangulo", 240, 100);
+	Rectangulo r(400, 300, 5, AZUL, 240, 100);
 	Circulo c(500, 500, AMARILLO, "Circulo", 50);
 	Cuadrado q(100, 100, ROJO, "Cuadrado", 100);
 	Elipse e(400, 300, VERDE, "Elipse", 150, 100);
-	vredimensiona(800, 600); // INICIALIZAR EL TAMAÑO DE LA PANTALLA
+	vredimensiona(800, 600); // INICIALIZAR EL TAMAÃ‘O DE LA PANTALLA
 	r.imprimir();
 	r.cambiarTamano(2);
 	q.imprimir();
