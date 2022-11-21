084436cbd5ee6f4eba4be190462b3d30d4e0fe83
Francisco Flores <91107008+FranciscoFlores725@users.noreply.github.com>
2022-11-20 23:14:42 -0300

    Tarea 2 - Francisco Flores- Maximiliano Quevedo -Tomas Mendicoa- Leonardo Fonseca
    Estimado Profesor
    
    El folder dice tarea 3, pero fue solo un error de tipeo. Lo presentado corresponde a la Tarea 2
    
    Saludos cordiales

---
+++ Tarea 2 Francisco Flores Maximiliano Quevedo Tomas Mendicoa Leonardo Fonseca/Tarea3/src/tarea3/Tarea3.java
@@ -0,0 +1,27 @@
+package tarea3;
+
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author franc
+ */
+public class Tarea3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                usuarios f= new usuarios();
+           f.setVisible(true);
+    }
+    
+}
