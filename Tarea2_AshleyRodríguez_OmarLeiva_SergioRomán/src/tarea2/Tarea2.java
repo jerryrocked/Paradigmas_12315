b44e2f690a69a4b0ec74808c92c79637bf5aa56b
ashleyvrs <114967325+ashleyvrs@users.noreply.github.com>
2022-11-20 21:48:56 -0300

    Tarea2_AshleyRodríguez_OmarLeiva_SergioRomán

---
+++ Tarea2_AshleyRodríguez_OmarLeiva_SergioRomán/src/tarea2/Tarea2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea2;
+
+/**
+ *
+ * @author ACA PON TU NOMBRE
+ */
+public class Tarea2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //new Registro().setVisible(true);
+        new Registro().setVisible(true);
+
+    }
+
+}
